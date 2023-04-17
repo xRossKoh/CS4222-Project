9982d6d4150a864af35fe2587b69c61b22961d22
xRossKoh <64789211+xRossKoh@users.noreply.github.com>
2023-04-17 20:44:33 +0800

    Delete build/cc26x0-cc13x0/sensortag/cc2650 directory

--- build/cc26x0-cc13x0/sensortag/cc2650/obj/.deps/sensor-common.d
+++
@@ -1,6 +0,0 @@
-build/cc26x0-cc13x0/sensortag/cc2650/obj/sensor-common.o: \
- ../../arch/platform/cc26x0-cc13x0/sensortag/sensor-common.c \
- ../../arch/platform/cc26x0-cc13x0/sensortag/sensor-common.h \
- ../../arch/platform/cc26x0-cc13x0/sensortag/board-i2c.h
-../../arch/platform/cc26x0-cc13x0/sensortag/sensor-common.h:
-../../arch/platform/cc26x0-cc13x0/sensortag/board-i2c.h:
--- build/cc26x0-cc13x0/sensortag/cc2650/obj/.deps/syscalls.d
+++
@@ -1,2 +0,0 @@
-build/cc26x0-cc13x0/sensortag/cc2650/obj/syscalls.o: \
- ../../os/lib/newlib/syscalls.c
